1cd8ee07f480e081c6bc7198530f6bd7517d9e1a
Daniel Tayler <danieltayler@mail.weber.edu>
2017-02-28 17:16:20 +0000

    init commit

---
+++ RealMadrid_hw6_task1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  RealMadrid_hw6_task1.c
+ *       	Usage:  ./RealMadrid_hw6_task1.c
+ *    Description:  :
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:07:32 PM
+ *
+ Compiler:  gcc -Wall -Werror -lm 
+ *         Author:  Daniel Tayler (), danieltayler@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int  main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
