9192abd9ef62b87fe8b9eac6fd12065a556d8352
Sharon Dansie <sharondansie@mail.weber.edu>
2017-03-02 17:07:43 +0000

    Usage Function done and parameters set

--- RealMadrid_hw6_task1.c
+++ RealMadrid_hw6_task1.c
@@ -22,6 +22,20 @@ int AskQuestion(void);
 /* Main Program */
 int  main(int argc, char *argv[])
 {
+	double ar1, ar2;
+	//Allow for 2 parameters
+	if (argc !=3)
+	{
+		Usage();
+	}
+	//Convert input parameters to float
+	ar1 = atof(argv[1]);
+	if (ar1 == 0.0)
+	{
+		Usage();
+	}
+	ar2 = atof(argv[2]);
+
 	int ans;
 	ShowIt(1,2);
 	ans = AskQuestion();
@@ -69,4 +83,13 @@ void ShowIt(double ra, double ang)
 
 
 	return;
+}
+
+void Usage(void)
+{
+	printf("Usage: ./polar x-coordinate y-coordinate\n");
+	printf("both parameters are required\n");
+	printf("must be a floating point\n");
+	printf("x-coordinate CANNOT = 0\n");
+	exit (1);
 }
