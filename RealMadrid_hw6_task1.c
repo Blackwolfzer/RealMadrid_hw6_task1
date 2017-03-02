52c36ea5efc4363b415bc2e6456aca678fe22f2c
Daniel Tayler <danieltayler@mail.weber.edu>
2017-03-02 03:14:44 +0000

    need to fix bug in funciton showit

--- RealMadrid_hw6_task1.c
+++ RealMadrid_hw6_task1.c
@@ -16,10 +16,16 @@
 #include <stdlib.h>
 
 /* Function Prototypes */
+void ShowIt(double ra, double ang);
+int AskQuestion(void);
 
 /* Main Program */
 int  main(int argc, char *argv[])
 {
+	int ans;
+	ShowIt(1,2);
+	ans = AskQuestion();
+	printf("%d\n",ans); // extra answer to check to see if function worked
 
 	return 0;
 }
@@ -28,3 +34,39 @@ int  main(int argc, char *argv[])
 /* Function Defenitions */
 
 
+int AskQuestion(void)
+{
+	int a;
+	while (1)
+	{
+		printf("Would you like to run this program againi (y/n):\n ");
+		printf("   Y = 1      N = 0  \n");
+		scanf("%d" , &a);
+		if (a == 1)
+		{
+			break;
+		}
+		else if (a == 0)
+		{
+			break;
+		}
+		else
+		{
+			printf("Your answer is incorrect plz try again\n");
+		}
+	}
+	
+	return a;
+}
+
+
+void ShowIt(double ra, double ang)
+{
+	double rad, angl;
+	rad = ra;
+	angl = ang;
+	printf("The radius is %lf\n and the angle is %lf\n" rad, angl);
+
+
+	return;
+}
